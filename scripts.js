f8b095ae82e50708a44a90330c5462d5b1a096ea
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2018-11-14 10:56:50 +0200

    Update scripts

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
-var imgcounter = 0;
+var CounterIMG = 0;
+
 function ChangeImage() {
-  if (imgcounter == 0)
-  {
-   document.getElementById("Image").src = "https://images.unsplash.com/photo-1533613220915-609f661a6fe1?ixlib=rb-0.3.5&s=3fda90f4984163cc30e13bdae325b382&auto=format&fit=crop&w=400&q=80";
-   }
-   else
-   {
-   document.getElementById("Image").src = "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-   imgcounter=0;
-   }
-   
+	if(CounterIMG == 0)
+{
+	document.getElementById("MyImage").src = "https://github.com/Luurkesien/luurkesien.github.io/blob/master/PICS/pic2.jpg";   
+	CounterIMG = 1;
+}
+	else
+{
+	document.getElementById("MyImage").src = "https://github.com/Luurkesien/luurkesien.github.io/blob/master/PICS/pic1.jpg";
+	CounterIMG = 0;
+}
 }
