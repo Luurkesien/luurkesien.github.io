c5701fc7344ce9bf575f215a76cf9cbc6fe95c3a
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2018-11-14 11:00:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,12 +3,12 @@ var CounterIMG = 0;
 function ChangeImage() {
 	if(CounterIMG == 0)
 {
-	document.getElementById("MyImage").src = "https://github.com/Luurkesien/luurkesien.github.io/blob/master/PICS/pic2.jpg";   
+	document.getElementById("MyImage").src = "/PICS/pic2.jpg";   
 	CounterIMG = 1;
 }
 	else
 {
-	document.getElementById("MyImage").src = "https://github.com/Luurkesien/luurkesien.github.io/blob/master/PICS/pic1.jpg";
+	document.getElementById("MyImage").src = "/PICS/pic1.jpg";
 	CounterIMG = 0;
 }
 }
