ff5998a7aa57eb89536f57756894cf449074f1dc
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2018-11-19 09:40:55 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 var CounterIMG = 0;
 
 function ChangeImage() {
+	
 	if(CounterIMG == 0)
 {
-	document.getElementById("MyImage").src = "/PICS/MySelf2.jpg";   
+	document.getElementById("MyImage").src = "/PICS/MySelf2.jpg";  
+	document.getElementById("ChangeButton").innerHTML = "Change to my business self.";
 	CounterIMG = 1;
 }
 	else
 {
 	document.getElementById("MyImage").src = "/PICS/MySelf.jpg";
+	document.getElementById("ChangeButton").innerHTML = "Change to my casual self.";
 	CounterIMG = 0;
 }
 }
