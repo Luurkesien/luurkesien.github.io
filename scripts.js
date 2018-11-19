63e2e2b87b0fb82804498b224e53d0b8dd392fd7
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2018-11-19 09:36:15 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,12 +3,12 @@ var CounterIMG = 0;
 function ChangeImage() {
 	if(CounterIMG == 0)
 {
-	document.getElementById("MyImage").src = "/PICS/pic2.jpg";   
+	document.getElementById("MyImage").src = "/PICS/MySelf2.jpg";   
 	CounterIMG = 1;
 }
 	else
 {
-	document.getElementById("MyImage").src = "/PICS/pic1.jpg";
+	document.getElementById("MyImage").src = "/PICS/MySelf.jpg";
 	CounterIMG = 0;
 }
 }
