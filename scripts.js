77a2a3628a9b61dd1c45d2534e9cacfedb1b1934
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2018-11-19 11:52:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,3 +15,42 @@ function ChangeImage() {
 	CounterIMG = 0;
 }
 }
+
+function HomeScreen()
+{
+	document.getElementById("MainScreen").innerHTML = "<h2>A lil bitty witty</h2>
+    <p> Heyo </p>
+    <p> I am the one, dont weight a ton ,etc. etc. </p>";
+	
+	
+}
+
+function BusinessScreen()
+{
+	document.getElementById("MainScreen").innerHTML = "<h2>business screeeen</h2>
+    <p>
+    <b>Links of interest:</b>
+  </p>
+  <p>
+    <a href="https://github.com/Luurkesien/luurkesien.github.io/tree/master/OpenScad">My OpenScad Projects</a>
+  </p>
+   <p>
+    <a href="https://github.com/Luurkesien/luurkesien.github.io/tree/master/Hardware">My Hardware Projects</a>
+  </p>
+   <p>
+    <a href="https://github.com/Luurkesien/luurkesien.github.io/tree/master/Software">My Software Projects</a>
+  </p>
+   <p>
+    <a href="https://github.com/Luurkesien/luurkesien.github.io/tree/master/Documents/MyCV.pdf">My CV</a>
+  </p>
+";
+	
+}
+
+function CasualScreen()
+{
+	document.getElementById("MainScreen").innerHTML = "<h2>casual screeen</h2>
+    <p> Heyo </p>
+    <p> I am the one, dont weight a ton ,etc. etc. </p>";
+	
+}
