a6067755a86d39d0f322db59314fe11f45ae3208
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2018-11-20 18:03:34 +0200

    1.1.1

---
+++ desenare.js
@@ -0,0 +1,23 @@
+document.getElementById("id_logic_version").innerHTML="Logic Version = 18.11.20.0";
+
+var canvas = document.getElementById("id_canvas");
+canvas.addEventListener("touchstart", on_touch);
+
+function on_touch()
+{
+	for (var i=0;i<e.changedTouchs.length;i++)
+	{
+		var context = canvas.getContext("2d");
+		context.beginPath();
+		context.arc(e.changedTouchs.item(i).pageX,
+					e.changedTouchs.item(i).pageY,
+					20,
+					0,2*Math.PI
+					);
+		
+		context.stroke();
+		
+	}
+	
+	
+}
