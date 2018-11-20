e7a869c85dd1ec4ff493a7a63538df125960a569
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2018-11-20 18:13:03 +0200

    1.1.2

--- desenare.js
+++ desenare.js
@@ -3,7 +3,7 @@ document.getElementById("id_logic_version").innerHTML="Logic Version = 18.11.20.
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
 
-function on_touch()
+function on_touch(e)
 {
 	for (var i=0;i<e.changedTouchs.length;i++)
 	{
