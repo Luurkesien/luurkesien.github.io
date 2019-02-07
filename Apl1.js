fb5f44011f2af7d9e0ef028c8528cd6ffbc2796c
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2019-02-07 21:30:12 +0200

    Update Apl1.js

--- Apl1.js
+++ Apl1.js
@@ -3,3 +3,10 @@ document.getElementById("id_logic_version").innerHTML="Logic Version = 2019.2.0"
 var svg = document.getElementById("id_svg");
 
 var svg_rect = svg.getBoundingClientRect();
+
+avg.addEventListener("touchstart", on_touch_svg);
+
+function on_touch_svg()
+{
+	console.log("atinge");
+}
