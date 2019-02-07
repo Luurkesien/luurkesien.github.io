4ac6eb45845deeb111abf4f71d7795f7d8d48c2a
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2019-02-07 21:23:55 +0200

    Update Apl1.js

--- Apl1.js
+++ Apl1.js
@@ -2,6 +2,4 @@ document.getElementById("id_logic_version").innerHTML="Logic Version = 2019.2.0"
 
 var svg = document.getElementById("id_svg");
 
-var svg_rect = svg.getBoundingClientRect():
-
-
+var svg_rect = svg.getBoundingClientRect();
