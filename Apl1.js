2cc6715f5f4e9437866ad8d666e1b4d2776fcb6d
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2019-02-07 22:07:58 +0200

    Update Apl1.js

--- Apl1.js
+++ Apl1.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML="Logic Version = 2019.2.5";
+document.getElementById("id_logic_version").innerHTML="Logic Version = 2019.2.6";
 
 var svg = document.getElementById("id_svg");
 var cerc1= document.getElementById("id_cerc1");
@@ -6,10 +6,13 @@ var cul = "blue";
 
 var svg_rect = svg.getBoundingClientRect();
 
-cerc1.addEventListener("touchstart", on_touch_svg);
+cerc1.addEventListener("touchstart", on_touch_color);
 
-function on_touch_svg()
+
+function on_touch_color()
 {
-	cerc1.setAttribute("fill",cul);
+	//cerc1.setAttribute("fill",cul);
+		cerc1.fill="blue";
+	
 }
 
