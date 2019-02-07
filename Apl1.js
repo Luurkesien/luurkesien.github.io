3e063345fe2dac88baf6b7a2ed95b2d3f3753bfc
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2019-02-07 22:20:41 +0200

    Update Apl1.js

--- Apl1.js
+++ Apl1.js
@@ -1,8 +1,9 @@
-document.getElementById("id_logic_version").innerHTML="Logic Version = 2019.2.8";
+document.getElementById("id_logic_version").innerHTML="Logic Version = 2019.2.9";
 
 var svg = document.getElementById("id_svg");
 var cerc1= document.getElementById("id_cerc1");
 var cul = "blue";
+<audio id="audio" src="http://www.soundjay.com/button/beep-07.wav" autoplay="false" ></audio>
 var sound = document.getElementById("audio");
           
 var svg_rect = svg.getBoundingClientRect();
@@ -13,7 +14,7 @@ cerc1.addEventListener("touchstart", on_touch_color);
 function on_touch_color()
 {
 	//cerc1.setAttribute("fill",cul);
-		cerc1.r="100";
+		cerc1.setAttribute=("r","100");
 		sound.play();
 	
 }
