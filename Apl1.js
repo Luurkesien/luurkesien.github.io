bb43e6ac0b55e0852e219b2687c5d0b07b06c9a8
Luurkesien <44406514+Luurkesien@users.noreply.github.com>
2019-02-11 11:53:37 +0200

    Add files via upload

--- Apl1.js
+++ Apl1.js
@@ -1,5 +1,5 @@
 
-document.getElementById("id_logic_version").innerHTML="Logic Version = 2019.2.14";
+document.getElementById("id_logic_version").innerHTML="Logic Version = 2019.2.15";
 
 var svg = document.getElementById("id_svg");
 
@@ -27,46 +27,92 @@ OhitD.addEventListener("touchstart", on_touch_CD);
 
 svg.addEventListener("touchmove", on_touch_move_svg);
 BassD.addEventListener("touchmove", on_touch_move_bass);
+SnarD.addEventListener("touchmove", on_touch_move_snar);
+ChitD.addEventListener("touchmove", on_touch_move_chit);
+OhitD.addEventListener("touchmove", on_touch_move_ohit;
 
 function on_touch_BD()
 {
+	if (endis == 0)
 		bassd.play();
 }
 
 function on_touch_SD()
 {
+	if (endis == 0)
 		snared.play();
 }
 
 function on_touch_OD()
 {
+	if (endis == 0)
 		ohitd.play();
 }
 
 function on_touch_CD()
 {
+	if (endis == 0)
 		chitd.play();
 }
 
 function EnDis_Edit()
 {
 	if (endis == 1)
+	{
 		endis = 0;
+	document.getElementById("EditButton").innerHTML="Edit on!";	
+	}
 	else
+	{
 		endis = 1;
+	document.getElementById("EditButton").innerHTML="Click to edit!";	
+	}
 }
 
 function on_touch_move_svg(e)
 {
+	if (endis == 1)
+	{
 	e.preventDefault();
 	svg.setAttribute("cx", e.changedTouches[0].pageX);
 	svg.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
+	}
 }
 
 function on_touch_move_bass(e)
 {
+	if (endis == 1)
+	{
 	e.preventDefault();
 	BassD.setAttribute("x", e.changedTouches[0].pageX);
 	BassD.setAttribute("y", e.changedTouches[0].pageY - svg_rect.top);
+	}
 }
 
+function on_touch_move_snar(e)
+{
+	if (endis == 1)
+	{
+	e.preventDefault();
+	SnarD.setAttribute("x", e.changedTouches[0].pageX);
+	SnarD.setAttribute("y", e.changedTouches[0].pageY - svg_rect.top);
+	}
+}
+function on_touch_move_chit(e)
+{
+	if (endis == 1)
+	{
+	e.preventDefault();
+	ChitD.setAttribute("x", e.changedTouches[0].pageX);
+	ChitD.setAttribute("y", e.changedTouches[0].pageY - svg_rect.top);
+	}
+}
+function on_touch_move_ohit(e)
+{
+	if (endis == 1)
+	{
+	e.preventDefault();
+	OhitD.setAttribute("x", e.changedTouches[0].pageX);
+	OhitD.setAttribute("y", e.changedTouches[0].pageY - svg_rect.top);
+	}
+}
